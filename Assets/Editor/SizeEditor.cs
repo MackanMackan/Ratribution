77176da79f898d587e903809becff22a88a131ba
mayawingardh <mayaandersson@hotmail.se>
2022-04-21 12:06:34 +0200

    Random Size

---
+++ Assets/Editor/SizeEditor.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEditor;
+
+public class SizeEditor : EditorWindow
+{
+	public string myString = "0";
+	public string myString2 = "0";
+
+	[MenuItem("Window/RandomSize")]
+	public static void SetupWindow()
+	{
+		var window = GetWindow<SizeEditor>("RandomSize");
+		window.minSize = new Vector2(250, 375);
+		window.maxSize = new Vector2(window.minSize.x + 10, window.minSize.y + 10);
+	}
+
+	public void OnGUI()
+	{
+		GUILayout.Label("Choose Random Rage", EditorStyles.boldLabel);
+		GUILayout.Space(20f);
+		myString = EditorGUILayout.TextField("Random Rage", myString);
+		myString2 = EditorGUILayout.TextField("Random Rage", myString);
+		GUILayout.Space(20f);
+
+		if (GUILayout.Button($"Give {Selection.transforms.Length} Random Size"))
+		{
+			foreach (Transform trans in Selection.transforms)
+			{
+				Undo.RecordObject(trans, $"Align With Ground: Ground Object '{trans.name}'");
+				RandomSize(trans);
+			}
+		}
+
+		GUILayout.Space(20f);
+		GUILayout.Label("Undo/Redo", EditorStyles.boldLabel);
+		GUILayout.Space(20f);
+
+		if (GUILayout.Button("Perform Undo"))
+		{
+			Undo.PerformUndo();
+		}
+
+		EditorGUILayout.Space();
+
+		if (GUILayout.Button("Perform Redo"))
+		{
+			Undo.PerformRedo();
+		}
+	}
+
+    private void RandomSize(Transform trans)
+    {
+		float randomScale = UnityEngine.Random.Range(float.Parse("myString"), float.Parse("myString2"));
+		Transform transform = trans.GetComponent<Transform>();
+		Undo.RecordObject(transform, "Changing size");
+
+		if (transform != null)
+		{
+			transform.localScale = Vector3.one * randomScale;
+		}
+	}
+}
