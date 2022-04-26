51ab9455c7c51fa0b93c5a3cffdac297618611cc
mayawingardh <mayaandersson@hotmail.se>
2022-04-26 10:05:48 +0200

    prefab landing

--- Assets/Scripts/Camera/CinemachineShake.cs
+++ Assets/Scripts/Camera/CinemachineShake.cs
@@ -6,10 +6,7 @@ using System.Collections.Generic;
 public class CinemachineShake : MonoBehaviour
 {
     public static CinemachineShake Instance { get; private set; }
-
-    float shakeTime;
-
-   public  CinemachineFreeLook cinemachineFreeLook;
+    public  CinemachineFreeLook cinemachineFreeLook;
 
     private void Awake()
     {
@@ -34,21 +31,15 @@ public class CinemachineShake : MonoBehaviour
     }
     public IEnumerator Shake(float amplitude, float frequency, float time)
     {
+        Debug.Log("SHAKE");
         Noise(amplitude, frequency);
         yield return new WaitForSeconds(time);
         Noise(0, 0);
     }
     void Noise(float amplitude, float frequency)
-    {       
+    {
+        Debug.Log("HEJ");
         cinemachineFreeLook.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_AmplitudeGain = amplitude;
         cinemachineFreeLook.GetRig(1).GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>().m_FrequencyGain = frequency;
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            StartCoroutine(Shake(20, 20, 0.5f));
-        }
     }
 }
