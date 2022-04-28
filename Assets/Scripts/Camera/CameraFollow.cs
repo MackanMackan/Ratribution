e0ba7ad748734f04b8b3aa4c47c040b9511864bb
mayawingardh <mayaandersson@hotmail.se>
2022-04-28 11:33:29 +0200

    Change camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -6,18 +6,18 @@ using Cinemachine;
 public class CameraFollow : MonoBehaviour
 {
     GameObject refPlayer;
-    CinemachineFreeLook cmCam;
+    CinemachineVirtualCamera vCam;
     Transform cameraPos;
     Transform introCam;
 
     private void Start()
     {
         cameraPos = GameObject.FindGameObjectWithTag("FP_Cam").GetComponent<Transform>();
-        cmCam = GetComponent<CinemachineFreeLook>();
+        vCam = GetComponent<CinemachineVirtualCamera>();
 
         refPlayer = GameObject.FindGameObjectWithTag("Player");
-        cmCam.Follow = refPlayer.transform;
-        cmCam.LookAt = refPlayer.transform;
+        vCam.Follow = refPlayer.transform;
+        vCam.LookAt = refPlayer.transform;
     }
 }
 
