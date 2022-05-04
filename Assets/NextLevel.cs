7a23cd9ea9cd9b07d3283933d86406bf4dac6be2
mayawingardh <mayaandersson@hotmail.se>
2022-05-04 10:57:32 +0200

    Levels/gate

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gate1;
+    [SerializeField]
+    GameObject gate2;
+
+    GetBuildingHealth getLevelHealth;
+
+    public GameObject winUI;
+
+    float levelHealth;
+    float speed = 10;
+    
+
+    private void Start()
+    {
+        getLevelHealth = FindObjectOfType<GetBuildingHealth>();
+        winUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (getLevelHealth.level == Level.Level_2)
+        { 
+            GateOpen(gate1);
+        }
+
+        if ( getLevelHealth.level == Level.Level_3)
+        {
+            GateOpen(gate2);
+        }
+
+        if (getLevelHealth.level == Level.Level_4 || Input.GetMouseButtonDown(1))
+        {
+            winUI.SetActive(true);
+        }
+    }
+
+    public void GateOpen(GameObject gate)
+    {
+        Debug.Log("Hallå");
+        gate.transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+}
