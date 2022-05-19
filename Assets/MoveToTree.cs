fa4f8b118795d439685e46d5a1d7ec721e6f44ce
mayawingardh <mayaandersson@hotmail.se>
2022-05-19 10:49:40 +0200

    treecam

--- Assets/MoveToTree.cs
+++ Assets/MoveToTree.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class MoveToTree : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    CinemachineVirtualCamera treeCam;
+    [SerializeField]
+    CinemachineVirtualCamera cmCam;
+    GameObject motherTree;
+    GameObject player;
+
+    private void Awake()
     {
-        
+        treeCam.enabled = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        motherTree = GameObject.FindGameObjectWithTag("MotherTree");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(CamToTree());
+        }
+    }
+
+    IEnumerator CamToTree()
+    {
+        treeCam.enabled = true;
+        cmCam.enabled = false;
+
+        yield return new WaitForSeconds(2);
+
+        treeCam.enabled = false;
+        cmCam.enabled = true;
+        Destroy(this.gameObject, 2);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(CamToTree());
+        }
     }
 }
