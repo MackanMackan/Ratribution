25b4026e9533f213207f37bc61ee750df0107a2c
mayawingardh <mayaandersson@hotmail.se>
2022-05-24 11:02:00 +0200

    Update Bilboard.cs

--- Assets/Scripts/Bilboard.cs
+++ Assets/Scripts/Bilboard.cs
@@ -7,21 +7,25 @@ public class Bilboard : MonoBehaviour
 {
     public CinemachineVirtualCamera vCamera;
     public bool bilboard;
-
+ 
     private void LateUpdate()
     {
+        Vector3 targetPostition = new Vector3(vCamera.transform.position.x, this.transform.position.y, vCamera.transform.position.z);
 
-        if (!bilboard)
-        {
-            transform.LookAt(vCamera.transform);
+        //if (!bilboard)
+        //{
+        //    transform.LookAt(vCamera.transform.position);
 
-        }
-        else
-        {
-            transform.rotation = vCamera.transform.rotation;
-        }
+        //}
+        //else
+        //{
+        //    transform.rotation = vCamera.transform.rotation;
+        //}
+
+        this.transform.LookAt(targetPostition);
 
         transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
     }
+
 }
 
